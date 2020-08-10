c5f27315d72dcd328c04bdead4af6be3b8f15726
Zun <zunsamuel97@gmail.com>
2020-08-10 16:58:15 -0500

    finished revArr

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -1,8 +1,38 @@
 package com.zun;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        String[] strArr = {"1", "2", "3", "4", "5"};
+        Integer[] intArr = {1,2,3,4,5};
+
+
+        revArr(intArr);
+        System.out.println("\n========");
+        revArr(strArr);
     }
+
+    private static void revArr(Object[] arr) {
+        List<Object> arrList = Arrays.asList(arr);
+        Collections.reverse(arrList);
+        arr = arrList.toArray();
+        print(arr);
+   }
+
+   private static void print(Object[] arr) {
+       System.out.print("[");
+       for(int i = 0; i < arr.length; i++){
+           String item = arr[i].toString();
+           if(arr[i] instanceof String) {
+               item = "'" + arr[i].toString() + "'";
+           }
+           String prompt = i != arr.length - 1 ? " " +  item + "," : " " + item + " ";
+           System.out.print(prompt);
+       }
+       System.out.print("]");
+   }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,10 @@
+const revArr = (arr: any[]) => arr.reverse();
+
 const main = () => {
-  console.log("banan");
+  const arr = [1, 2, 3, 4, 5];
+  const arr2 = ["1", "2", "3", "4", "5"];
+  console.log(revArr(arr));
+  console.log(revArr(arr2));
 };
 
 main();
