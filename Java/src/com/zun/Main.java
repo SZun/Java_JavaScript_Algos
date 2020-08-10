e747db303a4a79902c3dd92ef2de2bac0810fc35
Zun <zunsamuel97@gmail.com>
2020-08-09 21:45:42 -0500

    checked num vowels

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -1,8 +1,27 @@
 package com.zun;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        System.out.println(findNumVowels("hello"));
+        System.out.println(findNumVowels("why"));
     }
+
+    private static int findNumVowels(String str) {
+        int count = 0;
+        List<Character> arr = Arrays.asList('a', 'e', 'i', 'o', 'u');
+
+        char[] chars = str.toCharArray();
+
+        for(char i : chars) {
+            count = arr.contains(i) ? count + 1 : count;
+        }
+
+        return count;
+    }
+
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,17 @@
-const main = () => {
-  console.log("banan");
+const findNumVowels = (str: string): number => {
+  const vowelsArr = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    if (vowelsArr.indexOf(str[i]) !== -1) count++;
+  }
+
+  return count;
+};
+
+const main = (): void => {
+  console.log(findNumVowels("hello"));
+  console.log(findNumVowels("why"));
 };
 
 main();
