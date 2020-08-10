d2d7771191238de3bb09df2613173435258fcee9
Zun <zunsamuel97@gmail.com>
2020-08-09 22:15:52 -0500

    finished lower-upper

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -1,8 +1,47 @@
 package com.zun;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        String[] arr = {"a",
+                "b",
+                "c",
+                "d",
+                "e",
+                "f",
+                "g",
+                "h",
+                "i",
+                "j",
+                "k",
+                "l",
+                "m",
+                "n",
+                "o",
+                "p",
+                "q",
+                "r",
+                "s",
+                "t",
+                "u",
+                "v",
+                "w",
+                "x",
+                "y",
+                "z"};
+        System.out.println(getMapp(arr).toString());
+    }
+
+    private static Map<String,String> getMapp(String[] arr) {
+        Map<String,String> toReturn = new HashMap<>();
+
+        for(String i : arr){
+            toReturn.put(i, i.toUpperCase());
+        }
+
+        return toReturn;
     }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,41 @@
+const getMap = (arr: string[]): any => {
+  const obj = {};
+
+  arr.forEach((i) => (obj[i] = i.toUpperCase()));
+
+  return obj;
+};
+
 const main = () => {
-  console.log("banan");
+  const arr: string[] = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  console.log(getMap(arr));
 };
 
 main();
