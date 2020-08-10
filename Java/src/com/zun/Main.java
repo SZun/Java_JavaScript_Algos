a77d12647f36921cc8a819b4d61b07c429eedac5
Zun <zunsamuel97@gmail.com>
2020-08-09 21:10:07 -0500

    completedd fizzbuzz whiteboarding

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -1,8 +1,28 @@
 package com.zun;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        fizzBuzz(getArr());
+    }
+
+    private static void fizzBuzz(List<Integer> arr) {
+        arr.forEach(i -> {
+                if(i % 5 == 0 && i % 3 == 0) System.out.println("fizzbuzz");
+                else if(i % 5 == 0) System.out.println("fizz");
+                else if(i % 3 == 0) System.out.println("buzz");
+                else System.out.println(i);
+        });
+    }
+
+    private static List<Integer> getArr() {
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 1; i < 31; i++){
+            arr.add(i);
+        }
+        return arr;
     }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,31 @@
+const getArr = (): number[] => {
+  let arr: number[] = [];
+  for (var i = 1; i < 31; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const fizzBuzz = (arr: number[]) => {
+  arr.forEach((i) => {
+    switch (true) {
+      case i % 5 == 0 && i % 3 == 0:
+        console.log("fizzbuzz");
+        break;
+      case i % 5 == 0:
+        console.log("fizz");
+        break;
+      case i % 3 == 0:
+        console.log("buzz");
+        break;
+      default:
+        console.log(i);
+    }
+  });
+};
+
 const main = () => {
-  console.log("banan");
+  fizzBuzz(getArr());
 };
 
 main();
