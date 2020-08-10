48001c578f640a263c96675d7aea2da231a352f6
Zun <zunsamuel97@gmail.com>
2020-08-09 21:28:24 -0500

    completedd palindrome whiteboarding

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -3,6 +3,53 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        isPalindrome(getAllPalindromes());
+        System.out.println("====================");
+        isPalindrome(getAllNonPalidromes());
     }
+
+    private static void isPalindrome(String[] arr){
+        for(String i : arr) {
+            String iLower = i.toLowerCase();
+
+            StringBuilder input = new StringBuilder();
+            input.append(iLower);
+
+            String iReversed = input.reverse().toString();
+
+            System.out.println(i + " is a Palindrome ?: " + iLower.equals(iReversed));
+        }
+    }
+
+    private static String[] getAllPalindromes() {
+        String[] arr = {"Anna",
+                "Civic",
+                "Kayak",
+                "Level",
+                "Madam",
+                "Mom",
+                "Noon",
+                "Racecar"
+        };
+
+        return arr;
+    }
+
+    private static String[] getAllNonPalidromes() {
+        String[] arr = {
+                "scrape",
+                "minimum",
+                "circle",
+                "eject",
+                "glacier",
+                "cover",
+                "digital",
+                "pull",
+                "bark",
+                "fair"
+        };
+
+        return arr;
+    }
+
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,35 @@
+const isPalindrome = (str: string): boolean =>
+  str.toLowerCase() == str.toLowerCase().split("").reverse().join("");
+
 const main = () => {
-  console.log("banan");
+  let arr = [
+    "Anna",
+    "Civic",
+    "Kayak",
+    "Level",
+    "Madam",
+    "Mom",
+    "Noon",
+    "Racecar",
+  ];
+  arr.forEach((i) => console.log(`${i} is a Palindrome ?: ${isPalindrome(i)}`));
+
+  console.log("====================");
+
+  arr = [
+    "scrape",
+    "minimum",
+    "circle",
+    "eject",
+    "glacier",
+    "cover",
+    "digital",
+    "pull",
+    "bark",
+    "fair",
+  ];
+
+  arr.forEach((i) => console.log(`${i} is a Palindrome ?: ${isPalindrome(i)}`));
 };
 
 main();
