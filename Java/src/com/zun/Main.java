fd620c672213bd4cf9ec32db1890f5dc9161dea5
Zun <zunsamuel97@gmail.com>
2020-08-11 22:12:35 -0500

    completed unique sort

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -1,8 +1,38 @@
 package com.zun;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        Integer[] ints = {1, 2, 3, 4};
+        Integer[] ints2 = {1, 4, 4, 4, 4, 4, 3, 2, 1, 2};
+        Integer[] ints3 = {6, 7, 3, 2, 1};
+
+        print(uniqueSort(ints));
+        print(uniqueSort(ints2));
+        print(uniqueSort(ints3));
+    }
+
+    private static Object[] uniqueSort(Integer[] arr){
+
+        return new ArrayList<Integer>(
+                                        new HashSet<>(
+                                                Arrays.asList(arr)
+                                        )
+                                      ).toArray();
+    }
+
+    private static void print(Object[] arr) {
+        System.out.print("[");
+
+        for(Object i : arr){
+            System.out.print(" " + i.toString() + ", ");
+        }
+
+        System.out.print("]\n");
     }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,9 @@
+const uniqueSort = (arr: number[]): number[] => Array.from(new Set(arr)).sort();
+
 const main = () => {
-  console.log("banan");
+  console.log(uniqueSort([1, 2, 4, 3]));
+  console.log(uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]));
+  console.log(uniqueSort([6, 7, 3, 2, 1]));
 };
 
 main();
