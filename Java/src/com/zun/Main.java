15a2caa66cbca85582b4ea148e383fa4be28c655
Zun <zunsamuel97@gmail.com>
2020-08-11 22:33:23 -0500

    completed stand-in-line

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -1,8 +1,41 @@
 package com.zun;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(5, 6, 7, 8, 9));
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(7, 6, 3, 23, 17));
+        List<Integer> list3 = new ArrayList<>(Arrays.asList(1, 10, 20, 42));
+
+
+        print(standInLine(list1, 1));
+        print(standInLine(list2, 10));
+        print(standInLine(list3, 6));
+        print(standInLine(new ArrayList<>(), 6));
+    }
+
+    private static List<Integer> standInLine(List<Integer> list, Integer finalItem)  {
+
+        list.add(finalItem);
+        list.remove(0);
+
+        return list;
+    }
+
+    private static void print(List<Integer> list){
+        if(!list.isEmpty()){
+            System.out.print("[");
+
+            list.forEach(i -> System.out.print(" " +  i + ", "));
+
+            System.out.print("]\n");
+        } else {
+            System.out.println("No list has been selected");
+        }
+
     }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,15 @@
+const standInLine = (arr: number[], final: number): number[] | string => {
+  arr.push(final);
+  arr.shift();
+
+  return arr.length > 0 ? arr : "No list has been selected";
+};
+
 const main = () => {
-  console.log("banan");
+  console.log(standInLine([5, 6, 7, 8, 9], 1));
+  console.log(standInLine([7, 6, 3, 23, 17], 10));
+  console.log(standInLine([1, 10, 20, 42], 6));
+  console.log(standInLine([], 6));
 };
 
 main();
