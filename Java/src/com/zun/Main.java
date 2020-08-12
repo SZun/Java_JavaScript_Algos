68e12fbe77ab2b0cbd37dfee480f0c94e63122be
Zun <zunsamuel97@gmail.com>
2020-08-11 23:59:09 -0500

    finished enough change

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -3,6 +3,24 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+
+        double[] arr = {2, 100, 0, 0};
+        double[] arr2 = {0, 0, 20, 5};
+        double[] arr3 = {30, 40, 20, 5};
+        double[] arr4 = {10, 0, 0, 50};
+        double[] arr5 = {1, 0, 5, 219};
+
+        String prompt = "I have enough change?: ";
+        System.out.println(prompt + isEnoughChange(arr, 14.11));
+        System.out.println(prompt + isEnoughChange(arr2, 0.75));
+        System.out.println(prompt + isEnoughChange(arr3, 12.55));
+        System.out.println(prompt + isEnoughChange(arr4, 3.85));
+        System.out.println(prompt + isEnoughChange(arr5, 19.99));
+
+    }
+
+    private static boolean isEnoughChange(double[] arr, double price){
+        double total = (arr[0] * 0.25) + (arr[1] * 0.10) + (arr[2] * 0.05) + (arr[3] * 0.01);
+        return total >= price;
     }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,17 @@
+const isEnoughChange = (arr: number[], price: number): boolean => {
+  const total: number =
+    arr[0] * 0.25 + arr[1] * 0.1 + arr[2] * 0.05 + arr[3] * 0.01;
+  return total >= price;
+};
+
 const main = () => {
-  console.log("banan");
+  const prompt = "I have enough change?: ";
+
+  console.log(prompt + isEnoughChange([2, 100, 0, 0], 14.11));
+  console.log(prompt + isEnoughChange([0, 0, 20, 5], 0.75));
+  console.log(prompt + isEnoughChange([30, 40, 20, 5], 12.55));
+  console.log(prompt + isEnoughChange([10, 0, 0, 50], 3.85));
+  console.log(prompt + isEnoughChange([1, 0, 5, 219], 19.99));
 };
 
 main();
