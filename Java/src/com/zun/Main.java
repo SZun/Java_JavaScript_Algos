1fe554378d4dc65cdae4c65a612f60358d526376
Zun <zunsamuel97@gmail.com>
2020-08-11 23:23:14 -0500

    calc complete

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -3,6 +3,39 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        Calculator calc = new Calculator(10,5);
+
+        System.out.println(calc.add());
+        System.out.println(calc.subtract());
+        System.out.println(calc.multiply());
+        System.out.println(calc.divide());
     }
+
+    public static class Calculator {
+
+        private final int num1;
+        private final int num2;
+
+        public Calculator(int num1, int num2) {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public int add(){
+            return num1 + num2;
+        }
+
+        public int subtract(){
+            return num1 - num2;
+        }
+
+        public int multiply(){
+            return num1 * num2;
+        }
+
+        public int divide(){
+            return num1 / num2;
+        }
+    }
+
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,32 @@
+class Calculator {
+  private num1: number;
+  private num2: number;
+
+  constructor(num1, num2) {
+    this.num1 = num1;
+    this.num2 = num2;
+  }
+
+  add(): number {
+    return this.num1 + this.num2;
+  }
+  subtract(): number {
+    return this.num1 - this.num2;
+  }
+  divide(): number {
+    return this.num1 / this.num2;
+  }
+  multiply(): number {
+    return this.num1 * this.num2;
+  }
+}
+
 const main = () => {
-  console.log("banan");
+  var calc = new Calculator(10, 5);
+  console.log(calc.add());
+  console.log(calc.subtract());
+  console.log(calc.divide());
+  console.log(calc.multiply());
 };
 
 main();
