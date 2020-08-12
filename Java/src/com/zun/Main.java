3223b9e3c78c17dc9cf0589570c5b1e80857df53
Zun <zunsamuel97@gmail.com>
2020-08-11 21:48:33 -0500

    xo completed

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -3,6 +3,24 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        System.out.println(XO("ooxx"));
+        System.out.println(XO("xooxx"));
+        System.out.println(XO("ooxXm"));
+        System.out.println(XO("zpzpzpp"));
+        System.out.println(XO("zzoo"));
+    }
+
+    private static boolean XO(String str) {
+        int xCount = 0;
+        int oCount = 0;
+
+        char[] strArr = str.toCharArray();
+
+        for(char i : strArr){
+            if(Character.toLowerCase(i) == 'x') xCount++;
+            if(Character.toLowerCase(i) == 'o') oCount++;
+        }
+
+        return xCount == oCount;
     }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,21 @@
+const XO = (str: string): boolean => {
+  let xCount = 0;
+  let oCount = 0;
+
+  for (var i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() == "x") xCount++;
+    if (str[i].toLowerCase() == "o") oCount++;
+  }
+
+  return xCount == oCount;
+};
+
 const main = () => {
-  console.log("banan");
+  console.log(XO("ooxx"));
+  console.log(XO("xooxx"));
+  console.log(XO("ooxXm"));
+  console.log(XO("zpzpzpp"));
+  console.log(XO("zzoo"));
 };
 
 main();
