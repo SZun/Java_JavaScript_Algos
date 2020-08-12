c26f783d5d9ea2ad84e01de5e589c2557de7909c
Zun <zunsamuel97@gmail.com>
2020-08-11 23:01:32 -0500

    completed sort by length

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -3,6 +3,39 @@ package com.zun;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        String[] arr1 = {"Google", "Apple", "Microsoft"};
+        String[] arr2 = {"Leonardo", "Michelangelo", "Raphael", "Donatello"};
+        String[] arr3 = {"Turing", "Einstein", "Jung"};
+
+        print(bubbleSort(arr1));
+        print(bubbleSort(arr2));
+        print(bubbleSort(arr3));
     }
+
+    private static String[] bubbleSort(String[] arr){
+
+        for(int i = 0; i < arr.length - 1; i++){
+            for(int j = 0; j < arr.length - i - 1; j++){
+                if(arr[j].length() > arr[j + 1].length()) {
+                    String temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    private static void print(String[] arr) {
+
+        System.out.print("[");
+
+        for(String i : arr){
+            System.out.print(" '" + i + "', ");
+        }
+
+        System.out.print("]\n");
+    }
+
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,23 @@
+const sortByLength = (arr: string[]): string[] => {
+  // simple bubble sort
+  for (var i = 0; i < arr.length - 1; i++) {
+    for (var j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j].length > arr[j + 1].length) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+};
+
 const main = () => {
-  console.log("banan");
+  console.log(sortByLength(["Google", "Apple", "Microsoft"]));
+  console.log(
+    sortByLength(["Leonardo", "Michelangelo", "Raphael", "Donatello"])
+  );
+  console.log(sortByLength(["Turing", "Einstein", "Jung"]));
 };
 
 main();
