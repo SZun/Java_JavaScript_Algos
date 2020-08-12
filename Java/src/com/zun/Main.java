b6331957177d4d61d8435ccde54479b1d2b5b566
Zun <zunsamuel97@gmail.com>
2020-08-12 00:19:52 -0500

    calc profits

--- Java/src/com/zun/Main.java
+++ Java/src/com/zun/Main.java
@@ -1,8 +1,36 @@
 package com.zun;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("banan");
+        final String prompt = "Profit is: ";
+
+        System.out.println(prompt + new Item(new BigDecimal("32.67"), new BigDecimal("45.0"), new BigDecimal(1200)).getProfit());
+        System.out.println(prompt + new Item(new BigDecimal("225.89"), new BigDecimal("550.0"), new BigDecimal(100)).getProfit());
+        System.out.println(prompt + new Item(new BigDecimal("2.77"), new BigDecimal("7.95"), new BigDecimal(8500)).getProfit());
+    }
+
+    public static class  Item {
+
+        private final BigDecimal cost;
+
+        private final BigDecimal price;
+
+        private final BigDecimal inventory;
+
+        public Item(BigDecimal cost, BigDecimal price, BigDecimal inventory) {
+            this.cost = cost;
+            this.price = price;
+            this.inventory = inventory;
+        }
+
+        public BigDecimal getProfit(){
+            BigDecimal perItemProfit = price.subtract(cost);
+            BigDecimal profit = inventory.multiply(perItemProfit);
+            return profit.setScale(0, RoundingMode.HALF_UP);
+        }
     }
 }
--- JavaScript/index.ts
+++ JavaScript/index.ts
@@ -1,5 +1,25 @@
+class Item {
+  private cost: number;
+  private price: number;
+  private inventory: number;
+
+  constructor(cost: number, price: number, inventory: number) {
+    this.cost = cost;
+    this.price = price;
+    this.inventory = inventory;
+  }
+
+  getProfit(): number {
+    return Math.round((this.price - this.cost) * this.inventory);
+  }
+}
+
 const main = () => {
-  console.log("banan");
+  const prompt = "Profit is: ";
+
+  console.log(prompt + new Item(32.67, 45.0, 1200).getProfit());
+  console.log(prompt + new Item(225.89, 550.0, 100).getProfit());
+  console.log(prompt + new Item(2.77, 7.95, 8500).getProfit());
 };
 
 main();
